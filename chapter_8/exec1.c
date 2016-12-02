111a35899ca0a9f3b3b5b447d8f2c874a26e960e
Zhang Yuguo <zhangyuguo27@163.com>
2016-12-02 09:46:18 +0800

    begin studying chapter_8

---
+++ chapter_8/exec1.c
@@ -0,0 +1,17 @@
+/*
+ * exec1.c
+ * shows how easy it for a program to run a program
+ */
+#include <stdio.h>
+#include <stdlib.h>
+main()
+{
+  char *arglist[3];
+  arglist[0] = "ls";
+  arglist[1] = "-l";
+  arglist[2] = 0;
+  printf("*** About to exec ls -l\n");
+  execvp("ls", arglist);
+  printf("***ls is done.bye\n");
+}
+
